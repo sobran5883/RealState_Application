d9a380632e6383b097fd5fa6f9b2e66687812bb0
sobran rajak <sobran0621@gmail.com>
2023-10-16 17:26:06 +0530

    connected to dataBase

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+    .connect("mongodb+srv://sobran:sobran@mern-estate.yvvke0o.mongodb.net/?retryWrites=true&w=majority")
+    // .connect(process.env.MONGO)
+    .then(()=>{
+        console.log("Connected to MongoDG!");
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+
 const app=express();
 
 app.listen(3000, ()=>{
