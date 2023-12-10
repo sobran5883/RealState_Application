8595f2960d99da0506cddd763920f2701fadc075
sobran rajak <sobran0621@gmail.com>
2023-12-11 00:15:18 +0530

    add signout functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -34,6 +34,7 @@ export const signin = async (req, res, next)=>{                //here we getting
   }
 }
 
+
 export const google = async (req, res, next) => {
   try{
     const user = await User.findOne({email: req.body.email})
@@ -53,3 +54,13 @@ export const google = async (req, res, next) => {
     next(error)
   }
 }
+
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('User has been logged out!');
+  } catch (error) {
+    next(error);
+  }
+};
