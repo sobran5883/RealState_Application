86902b6d30441fe81f7577eeab66eb83e95ab7b9
sobran rajak <sobran0621@gmail.com>
2024-02-23 19:58:14 +0530

    adding sign-up page

--- client/src/pages/signUp.jsx
+++ client/src/pages/signUp.jsx
@@ -1,38 +1,103 @@
-import { useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+// import { useState} from 'react';
+// import {Link, useNavigate} from 'react-router-dom';
+// import OAuth from '../components/OAuth';
+
+
+// export default function SignUp() { 
+//   const [formData, setFormData] = useState({})
+//   const [error, setError] = useState(null);        //piece of state for error, null at beginning
+//   const [loading, setLoading] = useState(false);   //piece of state for loading, false at beginning
+//   const navigate = useNavigate();
+
+//   const handleChange = (e) =>{
+//     setFormData(
+//       {
+//         ...formData,                         //keeping form data
+//         [e.target.id]: e.target.value,       //whatever is changing set that one to its value
+//       }
+//     );
+//   };
+
+//   const handleSubmit = async (e)  => {
+//     e.preventDefault();                      //prevent refreshing the page
+//     try{
+//       setLoading(true);
+//       const res = await fetch('/api/auth/signup',
+//       {                                       //stringifing the formdata
+//         method:'POST',
+//         headers:{
+//           'Content-Type':'application/json',
+//         },
+//         body: JSON.stringify(formData),
+//       });  
+//       const data= await res.json();
+//       console.log(data);
+//       if(data.success===false){
+//         setLoading(false);
+//         setError(data.message);
+//         return;
+//       }
+//       setLoading(false);
+//       setError(null);
+//       navigate('/sign-in');
+//     }catch(error){
+//       setLoading(false)
+//       setError(error.message);
+//     }
+//   };
+//   return (
+//     <div className='p-3 max-w-lg mx-auto'>
+//       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+//       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+//         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
+//         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+//         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+//         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+//           {loading ? 'Loading...':'Sign Up'}
+//         </button>
+//         <OAuth/>
+//       </form>
+//       <div className='flex gap-2 mt-5'>
+//         <p>Have an account?</p>
+//         <Link to={"/sign-in"}>
+//                <span className='text-blue-700'>Sign in</span>
+//         </Link>
+//       </div>
+//       {error && <p className='text-red-500 mt-5'>{error}</p>}
+//     </div>
+//   )
+// }
+
+
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
 
-
-export default function SignUp() { 
-  const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null);        //piece of state for error, null at beginning
-  const [loading, setLoading] = useState(false);   //piece of state for loading, false at beginning
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
-  const handleChange = (e) =>{
-    setFormData(
-      {
-        ...formData,                         //keeping form data
-        [e.target.id]: e.target.value,       //whatever is changing set that one to its value
-      }
-    );
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
-
-  const handleSubmit = async (e)  => {
-    e.preventDefault();                      //prevent refreshing the page
-    try{
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
       setLoading(true);
-      const res = await fetch('/api/auth/signup',
-      {                                       //stringifing the formdata
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json',
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
-      });  
-      const data= await res.json();
+      });
+      const data = await res.json();
       console.log(data);
-      if(data.success===false){
+      if (data.success === false) {
         setLoading(false);
         setError(data.message);
         return;
@@ -40,8 +105,8 @@ export default function SignUp() {
       setLoading(false);
       setError(null);
       navigate('/sign-in');
-    }catch(error){
-      setLoading(false)
+    } catch (error) {
+      setLoading(false);
       setError(error.message);
     }
   };
@@ -49,27 +114,25 @@ export default function SignUp() {
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
-        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          {loading ? 'Loading...':'Sign Up'}
+        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
+        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80' >
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
         <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to={"/sign-in"}>
-               <span className='text-blue-700'>Sign in</span>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }
-
-
-
 
 
 
